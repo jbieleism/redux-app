93e759140a8119008fffa485f913259e9f5e0f53
Justin Biele <justin.biele@gmail.com>
2016-11-01 12:35:40 -0700

    [add] Render book-list to dom

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,8 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+
+class BookList extends Component {
 
   renderList() {
     return this.props.books.map((book) => {
@@ -10,11 +12,42 @@ export default class BookList extends Component {
 
   render() {
     return(
-
       <ul className="list-group col-sm-4">
         {this.renderList()}
       </ul>
-
     )
   }
 }
+
+
+function mapStateToProps(state) {
+  return {
+
+    books: state.books
+
+  }
+}
+
+export default connect(mapStateToProps)(BookList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
